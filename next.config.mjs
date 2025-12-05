21956f4fd73a21911992ff734f67ffb410ced16a
Saichand <63704295+Avanaganti-Saichand@users.noreply.github.com>
2025-12-05 12:39:11 -0600

    Configure basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
   output: "export",
+  basePath: "/Saichamd_J",
+  assetPrefix: "/Saichamd_J",
   images: {
     unoptimized: true,
   },
